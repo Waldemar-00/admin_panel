7fc36d4c67079c8ca92df66beb0f10c40a039858
uladzimir <01myit@gmail.com>
2023-11-22 20:52:35 +0300

    Added logic for active class.

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -14,6 +14,16 @@ const HeroesFilters = () => {
   const heroes = useSelector(state => state.heroes)
   const dispatch = useDispatch()
   function filterOfHeroes(e) {
+    const buttons = document.querySelectorAll('.f-r')
+    buttons.forEach(btn => {
+      if (btn.className.includes('active')) {
+        btn.className = btn.className.slice(0, btn.className.indexOf('active'))
+      } 
+    })
+    if (!e.target.className.includes('active')) {
+      e.target.className = e.target.className + ' active'
+    }
+    console.log(e.target.className)
     let array
     if (e.target.innerText === 'all') {
       array = [...heroes]
@@ -30,11 +40,11 @@ const HeroesFilters = () => {
             <div className="card-body">
                 <p className="card-text">Отфильтруйте героев по элементам</p>
                 <div className="btn-group">
-                  <button className="btn btn-outline-dark active" onClick={(e) => filterOfHeroes(e)}>{ filters[0] }</button>
-                  <button className="btn btn-danger" onClick={(e) => filterOfHeroes(e)}>{ filters[1] }</button>
-                  <button className="btn btn-primary" onClick={(e) => filterOfHeroes(e)}>{ filters[2] }</button>
-                  <button className="btn btn-success" onClick={(e) => filterOfHeroes(e)}>{ filters[3] }</button>
-                  <button className="btn btn-secondary" onClick={(e) => filterOfHeroes(e)}>{ filters[4] }</button>
+                  <button className="btn btn-outline-dark f-r active" onClick={(e) => filterOfHeroes(e)}>{ filters[0] }</button>
+                  <button className="btn btn-danger f-r" onClick={(e) => filterOfHeroes(e)}>{ filters[1] }</button>
+                  <button className="btn btn-primary f-r" onClick={(e) => filterOfHeroes(e)}>{ filters[2] }</button>
+                  <button className="btn btn-success f-r" onClick={(e) => filterOfHeroes(e)}>{ filters[3] }</button>
+                  <button className="btn btn-secondary f-r" onClick={(e) => filterOfHeroes(e)}>{ filters[4] }</button>
                 </div>
             </div>
         </div>
