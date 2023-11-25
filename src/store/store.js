48461ac301e5a28207651323a20dd5c486bb186e
uladzimir <01myit@gmail.com>
2023-11-25 15:35:25 +0300

    I used the applyMiddleware function to create the enhancer.

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,21 @@
-import { createStore, combineReducers, compose } from 'redux' 
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux' 
 import heroes from '../reducers/heroes'
 import filteres from '../reducers/filteres'
-// import reducer from '../reducers/reducer' 
-const enhancer = (createStore) => (...arg) => {
-  const store = createStore(...arg)
-  const prevDispatch = store.dispatch
-  store.dispatch = (action) => {
-    return typeof action === 'string' ?
-      prevDispatch({ type: action }) : prevDispatch(action)
-  }
-  return store
+
+const middleware = () => (next) => (action) => {      // the store is the first function ({ dispatch, getState })
+  return typeof action === 'string' ? next({ type: action }) : next(action) // next is a dispatch
 }
-const store = createStore(combineReducers({ heroes, filteres }), compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())) 
+// const enhancer = (createStore) => (...arg) => {
+  // const store = createStore(...arg)
+  // const prevDispatch = store.dispatch
+  // store.dispatch = (action) => {
+    // return typeof action === 'string' ?
+      // prevDispatch({ type: action }) : prevDispatch(action)
+  // }
+  // return store
+// }
+const store = createStore(combineReducers({ heroes, filteres }),
+  compose(applyMiddleware(middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+) 
 export default store 
