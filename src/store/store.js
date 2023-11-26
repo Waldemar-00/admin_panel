7de2a8904d07149c388b5a49ed26be5be0d241f6
uladzimir <01myit@gmail.com>
2023-11-26 16:17:56 +0300

    I used the cofigureStore function from @reduxjs/toolkit to create the store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,20 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux' 
+// import { createStore, combineReducers, compose, applyMiddleware } from 'redux' 
+import { configureStore } from '@reduxjs/toolkit'
 import heroes from '../reducers/heroes'
 import filteres from '../reducers/filteres'
-import ReduxThunk from 'redux-thunk'
+// import ReduxThunk from 'redux-thunk'
+// import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware'
 
-const middleware = () => (next) => (action) => {      // the store is the first function ({ dispatch, getState })
+
+const customStringMiddleware = () => (next) => (action) => {      // the store is the first function ({ dispatch, getState })
   return typeof action === 'string' ? next({ type: action }) : next(action) // next is a dispatch
 }
-// const enhancer = (createStore) => (...arg) => {
-  // const store = createStore(...arg)
-  // const prevDispatch = store.dispatch
-  // store.dispatch = (action) => {
-    // return typeof action === 'string' ?
-      // prevDispatch({ type: action }) : prevDispatch(action)
-  // }
-  // return store
-// }
-const store = createStore(combineReducers({ heroes, filteres }),
-  compose(applyMiddleware(ReduxThunk, middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-) 
+// const store = createStore(combineReducers({ heroes, filteres }),
+  // compose(applyMiddleware(ReduxThunk, middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = configureStore({
+  reducer: { heroes, filteres },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(customStringMiddleware),
+  devTools: process.env.NODE_ENV !== 'production', // or true for all 
+  
+})
 export default store 
