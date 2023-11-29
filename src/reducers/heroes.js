c4040fe38ebacb89c143ef778629c1f0337dff47
uladzimir <01myit@gmail.com>
2023-11-29 20:11:38 +0300

    Other writing of code when making createReducer.

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -7,12 +7,23 @@ const initialState = {
   heroesLoadingStatus: 'idle',
   url: "https://admin-panel-fcc34-default-rtdb.firebaseio.com/heroes.json",
 }
-const heroes = createReducer(initialState, builder => {
-  builder.addCase(heroesFetching, state => { state.heroesLoadingStatus = 'loading' })
-    .addCase(heroesFetched, (state, action) => { state.heroesLoadingStatus = 'idle'; state.heroes = action.payload })
-    .addCase(heroesFetchingError, state => { state.heroesLoadingStatus = 'error' })
-    .addCase(deleteHeroes, (state, action) => { state.heroes = action.payload })
-    .addCase(addHeroes, (state, action) => { state.heroes = action.payload })
-    .addDefaultCase(() => {})
-} )
+// const heroes = createReducer(initialState, builder => {
+  // builder.addCase(heroesFetching, state => { state.heroesLoadingStatus = 'loading' })
+    // .addCase(heroesFetched, (state, action) => { state.heroesLoadingStatus = 'idle'; state.heroes = action.payload })
+    // .addCase(heroesFetchingError, state => { state.heroesLoadingStatus = 'error' })
+    // .addCase(deleteHeroes, (state, action) => { state.heroes = action.payload })
+    // .addCase(addHeroes, (state, action) => { state.heroes = action.payload })
+    // .addDefaultCase(() => {})
+// } )
+const heroes = createReducer(initialState, {
+  [heroesFetching]: state => { state.heroesLoadingStatus = 'loading' },
+  [heroesFetched]: (state, action) => { state.heroesLoadingStatus = 'idle'; state.heroes = action.payload },
+  [heroesFetchingError]: state => { state.heroesLoadingStatus = 'error' },
+  [deleteHeroes]: (state, action) => { state.heroes = action.payload },
+  [addHeroes]: (state, action) => { state.heroes = action.payload },
+  },
+  [], //! Array with functions of comparison
+  state => state //! Default case
+)
+//! This sintaxis only for native JS! NOT WORK WITH TYPESCRIPT!!!!!!!!!!!!!!!
 export default heroes 
